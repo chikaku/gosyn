eab05ecfd5a6052686cb6492664cf44b1b77d69c
Kevin Chen <kevin@chenbc.me>
2021-12-05 02:40:49 +0800

    proc_macro: add `EnumIntoWrapped` procedural marco

--- shika_proc_macro/src/enum_into_wrapped.rs
+++ shika_proc_macro/src/enum_into_wrapped.rs
@@ -4,14 +4,14 @@ use quote::quote;
 use syn::DataEnum;
 use syn::Ident;
 
-pub(crate) struct EnumFromWrapped {
+pub(crate) struct EnumIntoWrapped {
     enum_name: Ident,
     enum_data: DataEnum,
 }
 
-impl EnumFromWrapped {
+impl EnumIntoWrapped {
     pub fn new(enum_name: Ident, enum_data: DataEnum) -> Self {
-        EnumFromWrapped {
+        EnumIntoWrapped {
             enum_name,
             enum_data,
         }
@@ -24,12 +24,17 @@ impl EnumFromWrapped {
             .map(|var| {
                 let var_name = &var.ident;
                 let enum_name = &self.enum_name;
-                let wrapped = get_wrapped_unnamed("EnumFromWrapped", enum_name, var.fields.clone());
+                let wrapped = get_wrapped_unnamed("EnumIntoWrapped", enum_name, var.fields.clone());
 
                 quote! {
-                    impl From<#wrapped> for #enum_name {
-                        fn from(inner: #wrapped) -> Self {
-                            Self::#var_name(inner)
+                    impl ::std::convert::TryFrom<#enum_name> for #wrapped {
+                        type Error = ();
+                    
+                        fn try_from(value: #enum_name) -> ::core::result::Result<Self, Self::Error> {
+                            match value {
+                                #enum_name::#var_name(res) => Ok(res),
+                                _ => Err(()),
+                            }
                         }
                     }
                 }
--- shika_proc_macro/src/lib.rs
+++ shika_proc_macro/src/lib.rs
@@ -2,16 +2,22 @@
 
 use proc_macro::TokenStream;
 use syn::parse_macro_input;
+use syn::punctuated::Punctuated;
+use syn::token::Comma;
 use syn::Data;
 use syn::DataEnum;
 use syn::DeriveInput;
+use syn::Field;
+use syn::Fields;
 use syn::Ident;
 
 mod enum_from;
 mod enum_from_wrapped;
+mod enum_into_wrapped;
 
 use enum_from::EnumFrom;
 use enum_from_wrapped::EnumFromWrapped;
+use enum_into_wrapped::EnumIntoWrapped;
 
 #[proc_macro_derive(EnumFrom, attributes(enum_from))]
 pub fn enum_from(input: TokenStream) -> TokenStream {
@@ -29,10 +35,30 @@ pub fn enum_from_wrapped(input: TokenStream) -> TokenStream {
     EnumFromWrapped::new(id, data).write_output().into()
 }
 
+#[proc_macro_derive(EnumIntoWrapped)]
+pub fn enum_into_wrapped(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let (id, data) = assert_enum("EnumIntoWrapped", input);
+    EnumIntoWrapped::new(id, data).write_output().into()
+}
+
 fn assert_enum(name: &str, input: DeriveInput) -> (Ident, DataEnum) {
     let ident = input.ident.clone();
     match input.data {
         Data::Enum(data) => (ident, data),
         _ => panic!("{} must be an enum to use {}", &ident, name),
+    }
+}
+
+fn get_wrapped_unnamed(
+    macro_name: &str,
+    enum_name: &Ident,
+    fields: Fields,
+) -> Punctuated<Field, Comma> {
+    let err = format!("{}: can not use {}", enum_name, macro_name);
+    match fields {
+        Fields::Unnamed(field) => field.unnamed,
+        Fields::Unit => panic!("{} with unit variant", err),
+        Fields::Named(_) => panic!("{} with named variant", err),
     }
 }
